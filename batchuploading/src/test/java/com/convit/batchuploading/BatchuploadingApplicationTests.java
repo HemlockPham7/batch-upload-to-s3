f36f6247bb60f354458660503cb4555a3708efde
Viet-Hoang Pham <157110579+HemlockPham7@users.noreply.github.com>
2025-12-24 15:46:20 +0700

    Initial set up

---
+++ batchuploading/src/test/java/com/convit/batchuploading/BatchuploadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.convit.batchuploading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchuploadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
